d37ed73b71f28d00462b01a69d6c9b328b570614
= <=>
2020-11-29 20:43:50 +0200

    ValidateEmailExists

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -12,17 +12,23 @@ const {error} = registeValidation(req.body)
 // res.send(error.details[0].message);
 if(error) return res.status(400).send(error.details[0].message);
 
+// checking if the user is already in the database 
+const emailExist = await User.findOne({email: req.body.email});
+if (emailExist) return res.status(400).send('Email already Exists');
 
 //save function
+// create a new user 
     const user = new User ({ 
         password: req.body.password,
         name: req.body.name,
         email: req.body.email
     });
 
+    //the code should wait until this make request is finished 
+
     try{
         const savedUser = await user.save();
-        // console.log("ttt")s
+        // console.log("ttt")
         res.send(savedUser); 
     }catch(err){ 
         res.status(400).send(err);
