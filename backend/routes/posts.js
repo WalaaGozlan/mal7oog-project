cedd816e486311f222b76fbf76b485dbfaa859e0
= <=>
2020-11-30 03:36:54 +0200

    Adding jsonwebtokens to our auth

---
+++ backend/routes/posts.js
@@ -0,0 +1,19 @@
+const router = require('express').Router();
+const verify = require('./verifyTokin.js');
+
+router.get('/',verify, (req,res)=>{
+    // res.json({
+    //     posts: {
+    //     title: 'my first post',
+    //     description: 'random data you shouldnt access without being logged in'
+    //     }
+    // });
+
+    res.send(req.user);
+    // User.findbyone({_id: req.user})
+});
+
+
+
+
+module.exports = router ;
