ff8f76029995fe07544a6baef1e7a4bfa1a65d12
tojan-bukhari <gitn12333@gmail.com>
2020-12-01 18:23:14 +0200

    make anew user-frontend

--- frontend/src/components/auth/Register.js
+++ frontend/src/components/auth/Register.js
@@ -1,4 +1,7 @@
 import React, {useState} from 'react'
+// import { useHistory } from "react-router-dom";
+import Usercontext from "../../context/UserContext"
+import axios from "axios";
 
 export default function Register() {
     const [email, setEmail] = useState();
@@ -6,20 +9,46 @@ export default function Register() {
     const [passwordCheck, setPasswordCheck] = useState();
     const [name, setName] = useState();
 
+    const { setUserData } = useContext(Usercontext)
+    const history = useHistory();
+
+    const submit = async (e) => {
+      e.preventDefault();
+      const newUser = {email, password ,passwordCheck, displayName};
+       await axios.post('http://localhost:3000/users/register', newUser); 
+       const loginRes = await axios.post('http://localhost:3000/users/register', {
+        email,
+        password,
+      });
+      setUserData({
+        token: loginRes.data.token,
+        user: loginRes.data.user,
+      });
+      localStorage.setItem("auth-token", loginRes.data.token);
+    };
+    
+
     return (
         <div>
            <h2>Register</h2> 
-           <form>
+           <form onSubmit={submit}>
                <label htmlFor="register-name">Name</label>
-               <input id="register-name" type="text" placeholder="Name"/>  
+               <input id="register-name" type="text" placeholder="Name"
+                onChange={ e => setName(e.target.value)}
+               />  
 
 
                <label htmlFor="register-email">Email</label>
-               <input id="register-email" type="email" placeholder="Email"/>
+               <input id="register-email" type="email" placeholder="Email"
+                 onChange={ e => setEmail(e.target.value)} />
 
                <label htmlFor="register-password">Password</label>
-               <input id="register-password" type="password" placeholder= "Password"/>
-               <input type="password" placeholder="Verify password"/>
+               <input id="register-password" type="password" placeholder= "Password"
+                 onChange={ e => setPassword(e.target.value)} 
+               />
+               <input type="password" placeholder="Verify password"
+                 onChange={ e => setPasswordCheck(e.target.value)}
+               />
 
                <input type="submit" value="Register"/>
            </form>
