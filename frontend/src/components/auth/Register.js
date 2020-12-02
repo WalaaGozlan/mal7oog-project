624b1867df0db418dfdf5698082cd84dd763d6b2
= <=>
2020-12-03 00:26:37 +0200

    login&register-forms,,IT'S-FINISHEDDD

--- frontend/src/components/auth/Register.js
+++ frontend/src/components/auth/Register.js
@@ -1,67 +1,59 @@
-import React from 'react'
 
+import React, {useState, useContext} from 'react';
+import { useHistory } from "react-router-dom";
+import usercontext from "../../context/userContext";
+import axios from "axios";
 export default function Register() {
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+    const [passwordCheck, setPasswordCheck] = useState();
+    const [name, setName] = useState();
+
+    const { setUserData } = useContext(usercontext)
+    const history = useHistory();
+
+    const submit = async (e) => {
+      e.preventDefault();
+      const newUser = {email, password ,passwordCheck, name};
+       await axios.post('http://localhost:1300/api/user/register', newUser); 
+       const loginRes = await axios.post('http://localhost:1300/api/user/login', {
+        email,
+        password,
+      });
+      setUserData({
+        token: loginRes.data.token,
+        user: loginRes.data.user,
+      });
+      localStorage.setItem("auth-token", loginRes.data.token);
+      history.push("/")
+    };
+    
+
     return (
-        <div>
-            Register
+        <div className="page">
+           <h2>Register</h2> 
+           <form onSubmit={submit}>
+           
+               <label htmlFor="register-name">Name</label>
+               <input id="register-name" type="text" placeholder="Name"
+                onChange={ e => setName(e.target.value)}
+               />  
+​
+​
+               <label htmlFor="register-email">Email</label>
+               <input id="register-email" type="email" placeholder="Email"
+                 onChange={ e => setEmail(e.target.value)} />
+​
+               <label htmlFor="register-password">Password</label>
+               <input id="register-password" type="password" placeholder= "Password"
+                 onChange={ e => setPassword(e.target.value)} 
+               />
+               <input type="password" placeholder="confirm password"
+                 onChange={ e => setPasswordCheck(e.target.value)}
+               />
+​
+               <input type="submit" value="Register"/>
+           </form>
         </div>
     )
 }
-
-
-// import React, {useState, useContext} from 'react';
-// import { useHistory } from "react-router-dom";
-// import usercontext from "../../context/userContext";
-// import axios from "axios";
-// export default function Register() {
-//     const [email, setEmail] = useState();
-//     const [password, setPassword] = useState();
-//     const [passwordCheck, setPasswordCheck] = useState();
-//     const [name, setName] = useState();
-
-//     const { setUserData } = useContext(usercontext)
-//     const history = useHistory();
-
-//     const submit = async (e) => {
-//       e.preventDefault();
-//       const newUser = {email, password ,passwordCheck, name};
-//        await axios.post('http://localhost:3000/users/register', newUser); 
-//        const loginRes = await axios.post('http://localhost:3000/users/register', {
-//         email,
-//         password,
-//       });
-//       setUserData({
-//         token: loginRes.data.token,
-//         user: loginRes.data.user,
-//       });
-//       localStorage.setItem("auth-token", loginRes.data.token);
-//     };
-    
-
-//     return (
-//         <div>
-//            <h2>Register</h2> 
-//            <form onSubmit={submit}>
-//                <label htmlFor="register-name">Name</label>
-//                <input id="register-name" type="text" placeholder="Name"
-//                 onChange={ e => setName(e.target.value)}
-//                />  
-// ​
-// ​
-//                <label htmlFor="register-email">Email</label>
-//                <input id="register-email" type="email" placeholder="Email"
-//                  onChange={ e => setEmail(e.target.value)} />
-// ​
-//                <label htmlFor="register-password">Password</label>
-//                <input id="register-password" type="password" placeholder= "Password"
-//                  onChange={ e => setPassword(e.target.value)} 
-//                />
-//                <input type="password" placeholder="Verify password"
-//                  onChange={ e => setPasswordCheck(e.target.value)}
-//                />
-// ​
-//                <input type="submit" value="Register"/>
-//            </form>
-//         </div>
-//     )
-// }
