698eb283fb15d9de3f71354ead6ddc902dcf679a
= <=>
2020-12-03 11:50:14 +0200

    register-notice

--- frontend/src/components/auth/Login.js
+++ frontend/src/components/auth/Login.js
@@ -2,17 +2,20 @@ import React, {useState, useContext} from 'react';
 import { useHistory } from "react-router-dom";
 import usercontext from "../../context/userContext";
 import axios from "axios";
+import ErrNotice from ".././msg/ErrNotice.js"
 
 
 export default function Login() {
     const [email, setEmail] = useState();
     const [password, setPassword] = useState();
+    const [error, setError] = useState();
 
     const { setUserData } = useContext(usercontext)
     const history = useHistory();
 
     const submit = async (e) => {
       e.preventDefault();
+      try{
       const loginUser = {email, password};
        const loginRes = await axios.post('http://localhost:1300/api/user/login', loginUser);
       setUserData({
@@ -21,12 +24,17 @@ export default function Login() {
       });
       localStorage.setItem("auth-token", loginRes.data.token);
       history.push("/") //Q
+    }catch(err){
+        err.response.data.msg && setError(err.response.data.msg);
+        console.log(err.response.data.msg)
+    }
     };
     
 
     return (
         <div className="page">
            <h2>Login</h2> 
+           {error && <ErrNotice message={error} clearError={()=> setError(undefined)} />}
            <form onSubmit={submit}>
            
                <label htmlFor="login-email">Email</label>
--- frontend/src/components/msg/ErrNotice.js
+++ frontend/src/components/msg/ErrNotice.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function ErrNotice(props) {
     return (
-        <div>
+        <div className="error-notice">
             <span>{props.message}</span>
             <button onClick = {props.clearError}>X</button>
         </div>
