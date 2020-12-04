112f0ea0c9be2882a073ce3089de79f8e48c1cf2
= <=>
2020-12-04 15:42:02 +0200

    calendar-component

--- frontend/src/components/auth/Login.js
+++ frontend/src/components/auth/Login.js
@@ -1,54 +1,59 @@
-import React, {useState, useContext} from 'react';
+import React, { useState, useContext } from 'react';
 import { useHistory } from "react-router-dom";
 import usercontext from "../../context/userContext";
 import axios from "axios";
 import ErrNotice from ".././msg/ErrNotice.js"
+// import {MDBContainer, MDBRow, MDBCol, MDBInput,MDBBtn } from 'mdbreact';
+// import 'bootstrap/dist/css/bootstrap.min.css'
+// import '@fortawesome/fontawesome-free/css/all.min.css';
+// import 'bootstrap-css-only/css/bootstrap.min.css';
+// import 'mdbreact/dist/css/mdb.css';
 
 
 export default function Login() {
-    const [email, setEmail] = useState();
-    const [password, setPassword] = useState();
-    const [error, setError] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [error, setError] = useState();
 
-    const { setUserData } = useContext(usercontext)
-    const history = useHistory();
+  const { setUserData } = useContext(usercontext)
+  const history = useHistory();
 
-    const submit = async (e) => {
-      e.preventDefault();
-      try{
-      const loginUser = {email, password};
-       const loginRes = await axios.post('http://localhost:1300/api/user/login', loginUser);
+  const submit = async (e) => {
+    e.preventDefault();
+    try {
+      const loginUser = { email, password };
+      const loginRes = await axios.post('http://localhost:1300/api/user/login', loginUser);
       setUserData({
         token: loginRes.data.token,
         user: loginRes.data.user,
       });
       localStorage.setItem("auth-token", loginRes.data.token);
       history.push("/") //Q
-    }catch(err){
-        err.response.data.msg && setError(err.response.data.msg);
-        console.log(err.response.data.msg)
+    } catch (err) {
+      err.response.data.msg && setError(err.response.data.msg);
+      console.log(err.response.data.msg)
     }
-    };
-    
+  };
 
-    return (
-        <div className="page">
-           <h2>Login</h2> 
-           {error && <ErrNotice message={error} clearError={()=> setError(undefined)} />}
-           <form onSubmit={submit}>
+
+  return (
+    <div className="page">
+       <h2>Login</h2> 
+       {error && <ErrNotice message={error} clearError={()=> setError(undefined)} />}
+       <form onSubmit={submit}>
+       
+           <label htmlFor="login-email">Email</label>
+           <input id="login-email" type="email" placeholder="Email"
+             onChange={ e => setEmail(e.target.value)} />
+​
+           <label htmlFor="login-password">Password</label>
+           <input id="login-password" type="password" placeholder= "Password"
+             onChange={ e => setPassword(e.target.value)} 
+           />
            
-               <label htmlFor="login-email">Email</label>
-               <input id="login-email" type="email" placeholder="Email"
-                 onChange={ e => setEmail(e.target.value)} />
 ​
-               <label htmlFor="login-password">Password</label>
-               <input id="login-password" type="password" placeholder= "Password"
-                 onChange={ e => setPassword(e.target.value)} 
-               />
-               
-​
-               <input type="submit" value="Login"/>
-           </form>
-        </div>
-    )
+           <input type="submit" value="Login"/>
+       </form>
+    </div>
+)
 }
--- frontend/src/components/auth/Register.js
+++ frontend/src/components/auth/Register.js
@@ -5,21 +5,21 @@ import usercontext from "../../context/userContext";
 import axios from "axios";
 import ErrNotice from ".././msg/ErrNotice.js"
 export default function Register() {
-    const [email, setEmail] = useState();
-    const [password, setPassword] = useState();
-    const [passwordCheck, setPasswordCheck] = useState();
-    const [name, setName] = useState();
-    const [error, setError] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [passwordCheck, setPasswordCheck] = useState();
+  const [name, setName] = useState();
+  const [error, setError] = useState();
 
-    const { setUserData } = useContext(usercontext)
-    const history = useHistory();
+  const { setUserData } = useContext(usercontext)
+  const history = useHistory();
 
-    const submit = async (e) => {
-      e.preventDefault();
-      try{
+  const submit = async (e) => {
+    e.preventDefault();
+    try{
       const newUser = {email, password ,passwordCheck, name};
-       await axios.post('http://localhost:1300/api/user/register', newUser); 
-       const loginRes = await axios.post('http://localhost:1300/api/user/login', {
+      await axios.post('http://localhost:1300/api/user/register', newUser);
+      const loginRes = await axios.post('http://localhost:1300/api/user/login', {
         email,
         password,
       });
@@ -28,41 +28,41 @@ export default function Register() {
         user: loginRes.data.user,
       });
       localStorage.setItem("auth-token", loginRes.data.token);
-      history.push("/");
+      history.push("/calender");
     }
     catch(err){
-        err.response.data.msg && setError(err.response.data.msg);
-        console.log(err.response.data.msg)
+      err.response.data.msg && setError(err.response.data.msg);
+      console.log(err.response.data.msg)
     }
-    };
-    
+  };
 
-    return (
-        <div className="page">
-           <h2>Register</h2> 
-           {error && <ErrNotice message={error} clearError={()=> setError(undefined)} />}
-           <form onSubmit={submit}>
-           
-               <label htmlFor="register-name">Name</label>
-               <input id="register-name" type="text" placeholder="Name"
-                onChange={ e => setName(e.target.value)}
-               />  
-​
-​
-               <label htmlFor="register-email">Email</label>
-               <input id="register-email" type="email" placeholder="Email"
-                 onChange={ e => setEmail(e.target.value)} />
-​
-               <label htmlFor="register-password">Password</label>
-               <input id="register-password" type="password" placeholder= "Password"
-                 onChange={ e => setPassword(e.target.value)} 
-               />
-               <input type="password" placeholder="confirm password"
-                 onChange={ e => setPasswordCheck(e.target.value)}
-               />
-​
-               <input type="submit" value="Register"/>
-           </form>
-        </div>
-    )
+
+  return (
+    <div className="page">
+      <h2>Register</h2>
+      {error && <ErrNotice message={error} clearError={()=> setError(undefined)} />}
+      <form onSubmit={submit}>
+
+        <label htmlFor="register-name">Name</label>
+        <input id="register-name" type="text" placeholder="Name"
+          onChange={ e => setName(e.target.value)}
+        />
+
+
+        <label htmlFor="register-email">Email</label>
+        <input id="register-email" type="email" placeholder="Email"
+  onChange={ e => setEmail(e.target.value)} />
+
+        <label htmlFor="register-password">Password</label>
+        <input id="register-password" type="password" placeholder= "Password"
+          onChange={ e => setPassword(e.target.value)}
+        />
+        <input type="password" placeholder="confirm password"
+          onChange={ e => setPasswordCheck(e.target.value)}
+        />
+        
+        <input type="submit" value="Register"/>
+      </form>
+    </div>
+  )
 }
