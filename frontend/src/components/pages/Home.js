a332cbcf2dd20fca5a716b8a28452ac431a1e1bb
= <=>
2020-12-05 03:02:37 +0200

    home-page

--- frontend/src/components/pages/Home.js
+++ frontend/src/components/pages/Home.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import img from "../img.jpg"
+import img from "../img1.jpg"
 
 export default function Home() {
     return (
         <div>
             <div>
-            <img src={img} alt="home img" hight={1400} width={1079}/>
+            <img src={img} alt="home img" hight={1400} width={1524}  />
         </div>
         </div>
     )
